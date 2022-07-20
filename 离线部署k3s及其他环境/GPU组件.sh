1bd2053d358e96c3167eeaa2c18063e3df0b09a6
chreadppp <corray8270@gmail.com>
2022-07-20 14:03:07 +0800

    Update GPU组件.sh
    update

--- 离线部署k3s及其他环境/GPU组件.sh
+++ 离线部署k3s及其他环境/GPU组件.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-cheack_env() {
-    nvidia-smi -L > /dev/null 2>&1 && info "nvidia gpu check[OK]"|| warn "nvidia gpu check[failed] (if need gpu ,please check nvidia gpu driver or hardware)!" 
-    
-
-}
-
-install_nvidia_support() {
-    kubectl  apply -f manifests/gpushare-schd-extender.yaml
-    sleep 5 && kubectl label node --all  gpushare=true && kubectl  apply -f manifests/gpushare-device-plugin.yaml && kubectl  apply -f manifests/nvidia-device-plugin.yaml
-}
 
 # 创建nvidia-container-runtime及k3s-gpu-share支持
 
@@ -96,78 +86,29 @@ EOF
 }
 EOF
 
-
-#修改k3s-service
-cat > /etc/systemd/system/k3s.service <<-EOF 
-[Unit]
-Description=Lightweight Kubernetes
-Documentation=https://k3s.io
-Wants=network-online.target
-After=network-online.target
-
-[Install]
-WantedBy=multi-user.target
-
-[Service]
-Type=notify
-EnvironmentFile=-/etc/default/%N
-EnvironmentFile=-/etc/sysconfig/%N
-EnvironmentFile=-/etc/systemd/system/k3s.service.env
-KillMode=process
-Delegate=yes
-# Having non-zero Limit*s causes performance problems due to accounting overhead
-# in the kernel. We recommend using cgroups to do container-local accounting.
-LimitNOFILE=1048576
-LimitNPROC=infinity
-LimitCORE=infinity
-TasksMax=infinity
-TimeoutStartSec=0
-Restart=always
-RestartSec=5s
-ExecStartPre=/bin/sh -xc '! /usr/bin/systemctl is-enabled --quiet nm-cloud-setup.service'
-ExecStartPre=-/sbin/modprobe br_netfilter
-ExecStartPre=-/sbin/modprobe overlay
-ExecStart=/usr/local/bin/k3s \
-    server \
-        '--disable=traefik' \
-        '--data-dir' \
-        '/data/rancher/k3s' \
-        '--write-kubeconfig' \
-        '/root/.kube/config' \
-        '--docker' \
-        '--kube-apiserver-arg=authorization-mode=Node,RBAC' \
-        '--kube-apiserver-arg=allow-privileged=true' \
-        'masquerade-all=true' \
-        '--kube-proxy-arg' \
-        'metrics-bind-address=0.0.0.0' \
-        '--kube-apiserver-arg=service-node-port-range=20000-40000' \
-        '--kubelet-arg=max-pods=300' \
-        '--kube-scheduler-arg=policy-config-file=/etc/kubernetes/scheduler-policy-config.json' \
-
-EOF
-
-}
-
-start_k3s_docker() {
-  systemctl daemon-reload
-  systemctl stop k3s
-  systemctl restart docker
-  systemctl start k3s
-  echo "restart all services successed!"
 }
 
 
-{
-  gpu_support_k3s
-  start_k3s_docker
-  install_nvidia_support
+install_nvidia_support() {
+    kubectl apply -f manifests/gpushare-schd-extender.yaml
+    sleep 5 && kubectl label node --all gpushare=true && kubectl apply -f manifests/gpushare-device-plugin.yaml && kubectl apply -f manifests/nvidia-device-plugin.yaml
 }
 
 
 
 
 
+gpu_support_k3s
 
 
+echo "重启k3s环境。"
+systemctl daemon-reload 
+systemctl stop k3s
+systemctl restart docker
+systemctl start k3s
 
+echo "添加gpu组件支持。"
 
+install_nvidia_support
+
+echo "完成！"
